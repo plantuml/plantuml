9e659de6046c4c6a302fc9bb7b3263b7d1cb12c6
Dominik Broj <dominik.broj@adesso.de>
2016-06-12 15:28:12 +0200

    replace package.html with package-info.java.
    According to the Oracle technotes, this is the preferred way:
    https://docs.oracle.com/javase/8/docs/technotes/tools/windows/javadoc.html#packagecomment

---
+++ src/main/java/net/sourceforge/plantuml/core/package-info.java
@@ -0,0 +1,29 @@
+/* ========================================================================
+ * PlantUML : a free UML diagram generator
+ * ========================================================================
+ *
+ * Project Info:  http://plantuml.com
+ *
+ * This file is part of PlantUML.
+ *
+ * PlantUML is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PlantUML distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License aint with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+/**
+ * Core classes used by PlantUML. This package the basic classes used by PlantUML. Classes in this package are supposed
+ * to be quite stable over time.
+ */
+package net.sourceforge.plantuml.core;
