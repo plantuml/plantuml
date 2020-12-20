11a93170427deedc975b76f96ae18103c823d88a
Arnaud Roques <plantuml@gmail.com>
2020-12-20 13:59:51 +0100

    Missing files

---
+++ src/gen/annotation/HasND_Rank.java
@@ -0,0 +1,11 @@
+package gen.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.CONSTRUCTOR, ElementType.METHOD })
+public @interface HasND_Rank {
+}
---
+++ src/gen/annotation/Reviewed.java
@@ -0,0 +1,13 @@
+package gen.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.CONSTRUCTOR, ElementType.METHOD })
+public @interface Reviewed {
+
+	String when();
+}
