e0dc805c1a95722a3e90f10b3cc5c267613dcf5f
matthew16550 <matthew@matthew16550.com>
2021-03-14 04:39:16 +1100

    wip

--- src/net/sourceforge/plantuml/security/SecurityUtils.java
+++ src/net/sourceforge/plantuml/security/SecurityUtils.java
@@ -66,7 +66,7 @@ public class SecurityUtils {
 		return current;
 	}
 
-	public static void setCurrent(SecurityProfile current) {
+	public static void getSecurityProfile(SecurityProfile current) {
 		SecurityUtils.current = current;
 	}
 
