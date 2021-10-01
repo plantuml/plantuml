1c1a2887a8f736fdbf49c2e3aa5186eee1a8e635
matthew16550 <matthew@matthew16550.com>
2021-10-01 18:25:05 +1000

    wip

--- test/net/sourceforge/plantuml/approvaltesting/ApprovalTestingTest.java
+++ test/net/sourceforge/plantuml/approvaltesting/ApprovalTestingTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.awt.image.BufferedImage;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -40,6 +41,11 @@ class ApprovalTestingTest {
 	})
 	void test_parameterized(String s, int i) {
 		approvalTesting.approve(s + i);
+	}
+
+	@RepeatedTest(value = 2, name = "repetition {currentRepetition}")
+	void test_repeated() {
+		approvalTesting.approve("foo");
 	}
 
 	@Test
