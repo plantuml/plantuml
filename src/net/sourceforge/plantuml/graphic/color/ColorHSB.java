ae4d0b65a21244280267c2334357c0631ba228b9
Arnaud Roques <plantuml@gmail.com>
2021-10-19 20:21:37 +0200

    Fix toString() method for non-US JVM

--- src/net/sourceforge/plantuml/graphic/color/ColorHSB.java
+++ src/net/sourceforge/plantuml/graphic/color/ColorHSB.java
@@ -1,14 +1,15 @@
 package net.sourceforge.plantuml.graphic.color;
 
 import java.awt.Color;
+import java.util.Locale;
 
 /**
  * {@link Color} with hue, saturation and brightness.
  */
 public class ColorHSB extends Color {
-	final float hue;
-	final float saturation;
-	final float brightness;
+	private final float hue;
+	private final float saturation;
+	private final float brightness;
 
 	public ColorHSB(int rgba) {
 		super(rgba, true);
@@ -36,7 +37,7 @@ public class ColorHSB extends Color {
 
 	@Override
 	public String toString() {
-		return String.format("%s[a=%02X r=%02X g=%02X b=%02X / h=%f s=%f b=%f]",
+		return String.format(Locale.US, "%s[a=%02X r=%02X g=%02X b=%02X / h=%f s=%f b=%f]",
 				getClass().getSimpleName(), getAlpha(), getRed(), getGreen(), getBlue(), getHue(), getSaturation(), getBrightness()
 		);
 	}
