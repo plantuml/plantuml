54d0bf27b58525095c309b5b893685c74348e65b
matthew16550 <matthew@matthew16550.com>
2021-11-13 13:55:54 +1100

    Update CI to create snapshot releases in GitHub

---
+++ .github/scripts/release_snapshot.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ex
+
+TAG="snapshot"
+
+gh release delete "${TAG}" -y || true
+
+git tag --force "${TAG}"
+
+git push --force origin "${TAG}"
+
+mv plantuml.jar         plantuml-SNAPSHOT.jar
+mv plantuml-javadoc.jar plantuml-SNAPSHOT-javadoc.jar
+mv plantuml-sources.jar plantuml-SNAPSHOT-sources.jar
+
+cat <<-EOF >notes.txt
+  This is a pre-release of the latest development work.
+  ⚠️  **It is not ready for general use** ⚠️
+EOF
+
+gh release create --prerelease --target "${GITHUB_SHA}" --notes-file notes.txt "${TAG}" \
+  plantuml-SNAPSHOT.jar \
+  plantuml-SNAPSHOT-javadoc.jar \
+  plantuml-SNAPSHOT-sources.jar
+
+echo "::notice title=::Snapshot released at ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/releases/tag/${TAG}"
