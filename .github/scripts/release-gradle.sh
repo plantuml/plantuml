d201e7de64d4f7c1c7d61de088dc9685bd1ac1e3
soloturn <soloturn@gmail.com>
2022-02-12 08:56:20 +0100

    gradle, store POM_VERSION in properties so it is taken everywhere.

--- .github/scripts/release-gradle.sh
+++ .github/scripts/release-gradle.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -ex
 
+POM_VERSION=$(gradle properties -q | grep "version:" | awk '{print $2}')
 RELEASE_DIR="build/github_release"
 
 mkdir "${RELEASE_DIR}"
 
-ln -s "../libs/plantuml.jar"             "${RELEASE_DIR}/plantuml-${POM_VERSION}.jar"
-ln -s "../libs/plantuml-javadoc.jar"     "${RELEASE_DIR}/plantuml-${POM_VERSION}-javadoc.jar"
-ln -s "../libs/plantuml-sources.jar"     "${RELEASE_DIR}/plantuml-${POM_VERSION}-sources.jar"
+ln -s "../libs/plantuml-${POM_VERSION}.jar"             "${RELEASE_DIR}/plantuml-${POM_VERSION}.jar"
+ln -s "../libs/plantuml-${POM_VERSION}-javadoc.jar"     "${RELEASE_DIR}/plantuml-${POM_VERSION}-javadoc.jar"
+ln -s "../libs/plantuml-${POM_VERSION}-sources.jar"     "${RELEASE_DIR}/plantuml-${POM_VERSION}-sources.jar"
 
 if [[ -e "build/publications/maven/module.json.asc" ]]; then
   # signatures are optional so that forked repos can release snapshots without needing a gpg signing key
-  ln -s "../libs/plantuml.jar.asc"                  "${RELEASE_DIR}/plantuml-${POM_VERSION}.jar.asc"
-  ln -s "../libs/plantuml-javadoc.jar.asc"          "${RELEASE_DIR}/plantuml-${POM_VERSION}-javadoc.jar.asc"
-  ln -s "../libs/plantuml-sources.jar.asc"          "${RELEASE_DIR}/plantuml-${POM_VERSION}-sources.jar.asc"
+  ln -s "../libs/plantuml-${POM_VERSION}.jar.asc"                  "${RELEASE_DIR}/plantuml-${POM_VERSION}.jar.asc"
+  ln -s "../libs/plantuml-${POM_VERSION}-javadoc.jar.asc"          "${RELEASE_DIR}/plantuml-${POM_VERSION}-javadoc.jar.asc"
+  ln -s "../libs/plantuml-${POM_VERSION}-sources.jar.asc"          "${RELEASE_DIR}/plantuml-${POM_VERSION}-sources.jar.asc"
 fi
 
 gh release create \
