2f1e84b65793795a3554b3eb160877bd2043e607
Daniel Santos <daniel.santos@pobox.com>
2023-02-22 02:25:56 -0600

    Mangle XmiClassDiagramStandard into something that might work
    This commit still needs clean up.
    
    We're walking the model hierarchically and adding elements. Probably
    doesn't fix associations yet.

--- src/net/sourceforge/plantuml/xmi/XmiClassDiagramStandard.java
+++ src/net/sourceforge/plantuml/xmi/XmiClassDiagramStandard.java
@@ -40,24 +40,90 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Element;
 
 import net.sourceforge.plantuml.baraye.Entity;
+import net.sourceforge.plantuml.baraye.IGroup;
+import net.sourceforge.plantuml.baraye.ILeaf;
 import net.sourceforge.plantuml.classdiagram.ClassDiagram;
+import net.sourceforge.plantuml.cucadiagram.LeafType;
 
 public class XmiClassDiagramStandard extends XmiClassDiagramAbstract implements XmlDiagramTransformer {
+	private int renderPackage(String name) {
+		s.push(document.createElement("UML:Package"));
+		s.peek().setAttribute("xmi.id", "pkg" + classDiagram.getUniqueSequence());
+		s.peek().setAttribute("name", name);
+		return 1;
+	}
+
+	/*
+     *       <UML:Namespace.ownedElement>
+     *         <UML:Package xmi.id='11A2' name='a'>
+     *           <UML:Namespace.ownedElement>
+     *             <UML:Class xmi.id='cl0001' name='ClassName'>
+     */
+	private int renderEntity(IEntity entity) {
+		int levels = 0;
+
+		//for (final IEntity ent : classDiagram.getLeafsvalues()) {
+		//for (final Entity ent : classDiagram.getEntityFactory().leafs()) {
+		if (entity == null) {
+			throw new NullPointerException("oops, entity is null");//levels += renderPackage()
+		}
+
+		LeafType leafType = entity.getLeafType();
+
+		// This may or may not be needed. It's possible for a logical hierarchy to not match a
+		// naming hierarchy.  If that's not the case in this code then please remove this check
+		// and always add the UML:Namespace
+		String name = entity.getDisplay().get(0).toString();
+		if (name != null && name != "") {
+			s.push(document.createElement("UML:Namespace.ownedElement"));
+			++levels;
+		}
+
+		switch (leafType) {
+		case EMPTY_PACKAGE:
+			levels += renderPackage(name);
+
+		//	return levels;
+		default:
+			levels += addEntityNode(entity);
+		}
+
+		return levels;
+	}
+
+	private void renderGroup(IEntity entity, IGroup parent) {
+		int levels = 0;
+		IGroup group = null;
+
+		if (entity != null)
+			levels = renderEntity(entity);
+
+		if (entity.isGroup())
+			group = (IGroup) entity;
+
+		for (final IEntity child : group.getLeafsDirect()) {
+
+			// UGLY: Stealing logic from EntityFactory.leafs(), breaking encapsulation.
+			//if (entity == null && entity.isGroup())
+			//	continue;
+			renderGroup(child, group);
+		}
+
+		for (int i = 0; i < levels; ++i)
+			s.pop();
+	}
 
 	public XmiClassDiagramStandard(ClassDiagram classDiagram) throws ParserConfigurationException {
 		super(classDiagram);
-
-		for (final Entity ent : classDiagram.getEntityFactory().leafs()) {
-			// if (fileFormat == FileFormat.XMI_ARGO && isStandalone(ent) == false) {
-			// continue;
-			// }
-			final Element cla = createEntityNode(ent);
-			if (cla == null) {
-				continue;
-			}
-			ownedElement.appendChild(cla);
-			done.add(ent);
+		IGroup rootGroup = classDiagram.getEntityFactory().getRootGroup();
+		for (ILeaf leaf : rootGroup.getLeafsDirect()) {
+			renderGroup(leaf, rootGroup);
 		}
+		//classDiagram.getRootGroup()classDiagram.leaf
+
+		//for (final IEntity ent : classDiagram.getLeafsvalues()) {}
+		//getLeafsDirect
+		//renderQuark(.getQuark());
 
 		// if (fileFormat != FileFormat.XMI_STANDARD) {
 		// for (final Link link : classDiagram.getLinks()) {
@@ -66,6 +132,34 @@ public class XmiClassDiagramStandard extends XmiClassDiagramAbstract implements 
 		// }
 	}
 
+	/*
+	for (final IEntity ent : classDiagram.getLeafsvalues()) {
+		-			// if (fileFormat == FileFormat.XMI_ARGO && isStandalone(ent) == false) {
+		-			// continue;
+		-			// }
+		 			final Element cla = createEntityNode(ent);
+		 			if (cla == null) {
+		 				continue;
+
+final String parentCode = entity.getQuark().getParent().toStringPoint();
+-
+-					if (parentCode.length() == 0)
+-						cla.setAttribute("namespace", CucaDiagramXmiMaker.getModel(classDiagram));
+-					else
+-						cla.setAttribute("namespace", parentCode);
+-
+-					break;
+-				default:
+-					continue;
+-				}
+-
+-			final Element cla = createEntityNode(ent);
+-			if (cla == null) {
+-				continue;
+-			}
+-			ownedElement.appendChild(cla);
+-			done.add(ent);
+*/
 	// private boolean isStandalone(IEntity ent) {
 	// for (final Link link : classDiagram.getLinks()) {
 	// if (link.getEntity1() == ent || link.getEntity2() == ent) {
