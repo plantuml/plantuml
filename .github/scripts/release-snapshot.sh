60aa5edca070b0f6d103a69443ed55f501fd6e4a
Arnaud Roques <plantuml@gmail.com>
2023-05-12 16:21:45 +0200

    chore: add MIT signatures

--- .github/scripts/release-snapshot.sh
+++ .github/scripts/release-snapshot.sh
@@ -31,6 +31,8 @@ if [[ -e "build/publications/maven/module.json.asc" ]]; then
   cp "build/libs/plantuml-${RELEASE_VERSION}-javadoc.jar.asc" "github_release/plantuml-SNAPSHOT-javadoc.jar.asc"
   cp "build/libs/plantuml-${RELEASE_VERSION}-sources.jar.asc" "github_release/plantuml-SNAPSHOT-sources.jar.asc"
   cp "build/libs/plantuml-pdf-${RELEASE_VERSION}.jar.asc" "github_release/plantuml-pdf-SNAPSHOT.jar.asc"
+  cp "plantuml-mit/build/libs/plantuml-mit-${RELEASE_VERSION}.jar.asc" "github_release/plantuml-mit-SNAPSHOT.jar.asc"
+  cp "plantuml-mit/build/libs/plantuml-mit-${RELEASE_VERSION}-sources.jar.asc" "github_release/plantuml-mit-SNAPSHOT-sources.jar.asc"
 fi
 
 echo -n "${DATE_TIME_UTC}" > "github_release/plantuml-SNAPSHOT.timestamp"
