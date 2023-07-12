29bcbfee89bc9df3ab3d2a40194604a157349896
Arnaud Roques <plantuml@gmail.com>
2023-07-12 17:58:33 +0200

    chore: prepare version 1.2023.10

--- src/net/sourceforge/plantuml/version/Version.java
+++ src/net/sourceforge/plantuml/version/Version.java
@@ -46,7 +46,7 @@ public class Version {
 
 	// Warning, "version" should be the same in gradle.properties and Version.java
 	// Any idea anyone how to magically synchronize those :-) ?
-	private static final String version = "1.2023.10beta3";
+	private static final String version = "1.2023.10";
 
 	public static String versionString() {
 		return version;
@@ -80,7 +80,7 @@ public class Version {
 	}
 
 	public static long compileTime() {
-		return 1686680484235L;
+		return 1689177247577L;
 	}
 
 	public static String compileTimeString() {
