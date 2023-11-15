8a1a9dfc8b1c24337af325fb5c93f2c98c1a3c3e
The-Lum <86879521+The-Lum@users.noreply.github.com>
2023-11-15 20:35:20 +0000

    docs: add `package-info.java` for `gen.annotation`  (smetana)
    - add `package-info.java` for `gen.annotation`
    - add `@see` on `h` and `smetana/core`

--- src/h/package-info.java
+++ src/h/package-info.java
@@ -4,7 +4,18 @@
  * of <a href="https://graphviz.org"target="_top">GraphViz/Dot</a> 
  * in <a href="https://plantuml.com/smetana02" target="_top">plantuml</a>.
  *
+ * <p>
+ * Port of the <code>C header</code> (<code>.h</code>) of Graphviz on Java:
+ * <ul>
+ * <li><code>EN</code> for <code>enumeration</code> or <code>enum</code></li>
+ * <li><code>ST</code> for <code>structure</code></li>
+ * </ul>
+ * </p>
+ * 
  * @see gen
+ * @see gen.annotation
+ * @see gen.lib
+ * @see gen.plugin.dot_layout
  * @see smetana.core
  * @see net.sourceforge.plantuml.sdot
  * 
