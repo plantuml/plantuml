78a32d2743247d8990944ccb9141fcadbe4ab107
The-Lum <86879521+The-Lum@users.noreply.github.com>
2023-11-17 10:31:18 +0000

    docs: add `package-info.java` and `readme.md`

---
+++ src/net/sourceforge/plantuml/flowdiagram/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes used to manage 
+ * <a href="http://alphadoc.plantuml.com/doc/markdown/en/flow-diagram" target="_top">
+ * Flow Diagram</a>.
+ *
+ */
+package net.sourceforge.plantuml.flowdiagram;
