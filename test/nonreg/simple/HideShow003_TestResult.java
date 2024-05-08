a807c24daecf5cc6a1730f712ca5bfd91ebdf81b
Dietrich Travkin <dietrich.travkin@solunar.de>
2024-05-08 12:40:51 +0200

    Add test cases for issues #1580 #1735 #1768

---
+++ test/nonreg/simple/HideShow003_TestResult.java
@@ -0,0 +1,296 @@
+package nonreg.simple;
+
+/*
+"""
+DPI: 96
+dimension: [ 200.5000 ; 91.0000 ]
+scaleFactor: 1.0000
+seed: 472044100062206941
+svgLinkTarget: _top
+hoverPathColorRGB: null
+preserveAspectRatio: none
+
+COMMENT: cluster foo
+RECTANGLE:
+  pt1: [ 60.5000 ; 4.0000 ]
+  pt2: [ 122.5000 ; 85.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: NULL_COLOR
+
+TEXT:
+  text: foo
+  position: [ 68.4838 ; 16.8889 ]
+  orientation: 0
+  font: SansSerif.bold/14 [BOLD]
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+COMMENT: cluster bar
+PATH:
+   - type: SEG_MOVETO
+     pt1: [ 2.5000 ; 0.0000 ]
+   - type: SEG_LINETO
+     pt1: [ 51.5113 ; 0.0000 ]
+   - type: SEG_ARCTO
+     radius: [ 3.7500 ; 3.7500 ]
+     angle: 0.0
+     largeArcFlag: false
+     sweepFlag: true
+     dest: [ 54.0113 ; 2.5000 ]
+   - type: SEG_LINETO
+     pt1: [ 61.0113 ; 20.0000 ]
+   - type: SEG_LINETO
+     pt1: [ 61.5000 ; 20.0000 ]
+   - type: SEG_ARCTO
+     radius: [ 2.5000 ; 2.5000 ]
+     angle: 0.0
+     largeArcFlag: false
+     sweepFlag: true
+     dest: [ 64.0000 ; 22.5000 ]
+   - type: SEG_LINETO
+     pt1: [ 64.0000 ; 78.5000 ]
+   - type: SEG_ARCTO
+     radius: [ 2.5000 ; 2.5000 ]
+     angle: 0.0
+     largeArcFlag: false
+     sweepFlag: true
+     dest: [ 61.5000 ; 81.0000 ]
+   - type: SEG_LINETO
+     pt1: [ 2.5000 ; 81.0000 ]
+   - type: SEG_ARCTO
+     radius: [ 2.5000 ; 2.5000 ]
+     angle: 0.0
+     largeArcFlag: false
+     sweepFlag: true
+     dest: [ 0.0000 ; 78.5000 ]
+   - type: SEG_LINETO
+     pt1: [ 0.0000 ; 2.5000 ]
+   - type: SEG_ARCTO
+     radius: [ 2.5000 ; 2.5000 ]
+     angle: 0.0
+     largeArcFlag: false
+     sweepFlag: true
+     dest: [ 2.5000 ; 0.0000 ]
+  stroke: 0.0-0.0-1.5
+  shadow: 0
+  color: ff000000
+  backcolor: NULL_COLOR
+
+LINE:
+  pt1: [ 130.5000 ; 24.0000 ]
+  pt2: [ 191.5113 ; 24.0000 ]
+  stroke: 0.0-0.0-1.5
+  shadow: 0
+  color: ff000000
+
+TEXT:
+  text: bar
+  position: [ 134.5000 ; 16.8889 ]
+  orientation: 0
+  font: SansSerif.bold/14 [BOLD]
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+COMMENT: class C
+RECTANGLE:
+  pt1: [ 6.0000 ; 29.0000 ]
+  pt2: [ 51.2438 ; 77.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 6.0000 ; 29.0000 ]
+  pt2: [ 32.0000 ; 61.0000 ]
+
+ELLIPSE:
+  pt1: [ 10.0000 ; 34.0000 ]
+  pt2: [ 32.0000 ; 56.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 21.0000 ; 45.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 32.0000 ; 38.0000 ]
+  pt2: [ 51.2438 ; 52.0000 ]
+
+TEXT:
+  text: C
+  position: [ 35.0000 ; 48.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+LINE:
+  pt1: [ 7.0000 ; 61.0000 ]
+  pt2: [ 50.2438 ; 61.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 6.0000 ; 61.0000 ]
+  pt2: [ 18.0000 ; 69.0000 ]
+
+LINE:
+  pt1: [ 7.0000 ; 69.0000 ]
+  pt2: [ 50.2438 ; 69.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 6.0000 ; 69.0000 ]
+  pt2: [ 18.0000 ; 77.0000 ]
+
+COMMENT: class B
+RECTANGLE:
+  pt1: [ 69.0000 ; 29.0000 ]
+  pt2: [ 114.2419 ; 77.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 69.0000 ; 29.0000 ]
+  pt2: [ 95.0000 ; 61.0000 ]
+
+ELLIPSE:
+  pt1: [ 73.0000 ; 34.0000 ]
+  pt2: [ 95.0000 ; 56.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 84.0000 ; 45.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 95.0000 ; 38.0000 ]
+  pt2: [ 114.2419 ; 52.0000 ]
+
+TEXT:
+  text: B
+  position: [ 98.0000 ; 48.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+LINE:
+  pt1: [ 70.0000 ; 61.0000 ]
+  pt2: [ 113.2419 ; 61.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 69.0000 ; 61.0000 ]
+  pt2: [ 81.0000 ; 69.0000 ]
+
+LINE:
+  pt1: [ 70.0000 ; 69.0000 ]
+  pt2: [ 113.2419 ; 69.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 69.0000 ; 69.0000 ]
+  pt2: [ 81.0000 ; 77.0000 ]
+
+COMMENT: class A
+RECTANGLE:
+  pt1: [ 140.0000 ; 29.0000 ]
+  pt2: [ 185.2425 ; 77.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 140.0000 ; 29.0000 ]
+  pt2: [ 166.0000 ; 61.0000 ]
+
+ELLIPSE:
+  pt1: [ 144.0000 ; 34.0000 ]
+  pt2: [ 166.0000 ; 56.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 155.0000 ; 45.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 166.0000 ; 38.0000 ]
+  pt2: [ 185.2425 ; 52.0000 ]
+
+TEXT:
+  text: A
+  position: [ 169.0000 ; 48.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+LINE:
+  pt1: [ 141.0000 ; 61.0000 ]
+  pt2: [ 184.2425 ; 61.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 140.0000 ; 61.0000 ]
+  pt2: [ 152.0000 ; 69.0000 ]
+
+LINE:
+  pt1: [ 141.0000 ; 69.0000 ]
+  pt2: [ 184.2425 ; 69.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 140.0000 ; 69.0000 ]
+  pt2: [ 152.0000 ; 77.0000 ]
+
+"""
+*/
+public class HideShow003_TestResult {
+}
---
+++ test/nonreg/simple/HideShow004_TestResult.java
@@ -0,0 +1,217 @@
+package nonreg.simple;
+
+/*
+"""
+DPI: 96
+dimension: [ 339.7425 ; 140.0000 ]
+scaleFactor: 1.0000
+seed: -869984177947488494
+svgLinkTarget: _top
+hoverPathColorRGB: null
+preserveAspectRatio: none
+
+COMMENT: class B
+RECTANGLE:
+  pt1: [ 6.0000 ; 12.0000 ]
+  pt2: [ 270.1206 ; 66.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 112.1894 ; 12.0000 ]
+  pt2: [ 138.1894 ; 44.0000 ]
+
+ELLIPSE:
+  pt1: [ 116.1894 ; 17.0000 ]
+  pt2: [ 138.1894 ; 39.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 127.1894 ; 28.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 144.6894 ; 21.0000 ]
+  pt2: [ 163.9312 ; 35.0000 ]
+
+TEXT:
+  text: B
+  position: [ 147.6894 ; 31.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+LINE:
+  pt1: [ 7.0000 ; 44.0000 ]
+  pt2: [ 269.1206 ; 44.0000 ]
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+
+EMPTY:
+  pt1: [ 6.0000 ; 44.0000 ]
+  pt2: [ 270.1206 ; 66.0000 ]
+
+POLYGON:
+  points:
+   - [ 17.0000 ; 52.5000 ]
+   - [ 13.0000 ; 58.5000 ]
+   - [ 21.0000 ; 58.5000 ]
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff1963a0
+  backcolor: ff4177af
+
+TEXT:
+  text: run(): boolean
+  position: [ 26.0000 ; 58.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+COMMENT: class A
+RECTANGLE:
+  pt1: [ 288.5000 ; 23.0000 ]
+  pt2: [ 333.7425 ; 55.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 288.5000 ; 23.0000 ]
+  pt2: [ 314.5000 ; 55.0000 ]
+
+ELLIPSE:
+  pt1: [ 292.5000 ; 28.0000 ]
+  pt2: [ 314.5000 ; 50.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 303.5000 ; 39.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 314.5000 ; 32.0000 ]
+  pt2: [ 333.7425 ; 46.0000 ]
+
+TEXT:
+  text: A
+  position: [ 317.5000 ; 42.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+COMMENT: class C
+RECTANGLE:
+  pt1: [ 115.5000 ; 102.0000 ]
+  pt2: [ 160.7438 ; 134.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 115.5000 ; 102.0000 ]
+  pt2: [ 141.5000 ; 134.0000 ]
+
+ELLIPSE:
+  pt1: [ 119.5000 ; 107.0000 ]
+  pt2: [ 141.5000 ; 129.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 130.5000 ; 118.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 141.5000 ; 111.0000 ]
+  pt2: [ 160.7438 ; 125.0000 ]
+
+TEXT:
+  text: C
+  position: [ 144.5000 ; 121.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+COMMENT: class D
+RECTANGLE:
+  pt1: [ 178.5000 ; 102.0000 ]
+  pt2: [ 223.7431 ; 134.0000 ]
+  xCorner: 5
+  yCorner: 5
+  stroke: 0.0-0.0-0.5
+  shadow: 0
+  color: ff181818
+  backcolor: fff1f1f1
+
+EMPTY:
+  pt1: [ 178.5000 ; 102.0000 ]
+  pt2: [ 204.5000 ; 134.0000 ]
+
+ELLIPSE:
+  pt1: [ 182.5000 ; 107.0000 ]
+  pt2: [ 204.5000 ; 129.0000 ]
+  start: 0.0
+  extend: 0.0
+  stroke: 0.0-0.0-1.0
+  shadow: 0
+  color: ff181818
+  backcolor: ffadd1b2
+
+CENTERED_CHAR:
+  char: C
+  position: [ 193.5000 ; 118.0000 ]
+  font: Monospaced.bold/17
+  color: ff000000
+
+EMPTY:
+  pt1: [ 204.5000 ; 111.0000 ]
+  pt2: [ 223.7431 ; 125.0000 ]
+
+TEXT:
+  text: D
+  position: [ 207.5000 ; 121.8889 ]
+  orientation: 0
+  font: SansSerif.plain/14 []
+  color: ff000000
+  extendedColor: NULL_COLOR
+
+"""
+*/
+public class HideShow004_TestResult {
+}
