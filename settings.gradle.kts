eaf608ff877cd8439273427aae2ef917254e3b96
PlantUML <plantuml@gmail.com>
2024-08-26 09:43:18 +0000

    chore: gplv2 for java 11 or higher

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,25 @@ val isCiBuild = System.getenv("CI") != null
 val version: String by settings
 
 println("Running settings.gradle.kts")
-// println(rootProject.projectDir)
 println("Version is " + version)
 
-if (isCiBuild) {
-	include("plantuml-asl")
-	include("plantuml-bsd")
-	include("plantuml-epl")
-	include("plantuml-lgpl")
-	include("plantuml-mit")
-	include("plantuml-gplv2")
-} else {
-	println("Not a CI build: only GPL will be generated")
-}
+// Check Java version
+val javaVersion = JavaVersion.current()
+println("Current Java version is " + javaVersion)
 
+if (isCiBuild) {
+    include("plantuml-asl")
+    include("plantuml-bsd")
+    include("plantuml-epl")
+    include("plantuml-lgpl")
+    include("plantuml-mit")
+    
+    // Only include plantuml-gplv2 if Java version is 11 or higher
+    if (javaVersion.isCompatibleWith(JavaVersion.VERSION_11)) {
+        include("plantuml-gplv2")
+    } else {
+        println("Skipping plantuml-gplv2 as it requires Java 11 or higher")
+    }
+} else {
+    println("Not a CI build: only GPL will be generated")
+}
