050530fa1ec97fce5c029f1de09d93faba753882
Arnaud Roques <plantuml@gmail.com>
2024-10-09 21:55:00 +0200

    issue

---
+++ test/dev/ebnf_3.java
@@ -0,0 +1,59 @@
+package dev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.GeneratedImage;
+import net.sourceforge.plantuml.SourceFileReader;
+import net.sourceforge.plantuml.preproc.Defines;
+
+/*
+ * 
+
+@startebnf
+title First [modified] example of §5.8 Syntactic-term of ISO-EBNF
+
+letter = ? "A" - "Z" ?;
+
+vowel = "A" | "E" | "I" | "O" | "U";
+
+consonant = letter - vowel;
+@endebnf
+
+
+
+
+ */
+public class ebnf_3 {
+
+	protected File getJavaFile() {
+		final String name = getClass().getName();
+		final File f = new File("test/" + name.replace('.', '/') + ".java");
+		return f;
+	}
+
+	@Test
+	public void testExecute() throws IOException, InterruptedException {
+		final File file = getJavaFile();
+		final FileFormatOption options = new FileFormatOption(FileFormat.PNG);
+
+		final File outputDirectory = new File("outputdev", "png").getAbsoluteFile();
+		outputDirectory.mkdirs();
+
+		final SourceFileReader reader = new SourceFileReader(Defines.createWithFileName(file), file, outputDirectory,
+				Collections.<String>emptyList(), "UTF-8", options);
+		final List<GeneratedImage> list = reader.getGeneratedImages();
+
+		assertEquals(1, list.size());
+
+	}
+
+}
