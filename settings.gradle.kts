6286df12099de17af1190c6899bdb9732771b4b9
Arnaud Roques <plantuml@gmail.com>
2025-03-09 16:23:04 +0100

    temporary skip asl and other build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,18 +13,18 @@ val javaVersion = JavaVersion.current()
 println("Current Java version is " + javaVersion)
 
 if (isCiBuild) {
-    include("plantuml-asl")
-    include("plantuml-bsd")
-    include("plantuml-epl")
-    include("plantuml-lgpl")
-    include("plantuml-mit")
+//    include("plantuml-asl")
+//    include("plantuml-bsd")
+//    include("plantuml-epl")
+//    include("plantuml-lgpl")
+//    include("plantuml-mit")
     
     // Only include plantuml-gplv2 if Java version is 11 or higher
-    if (javaVersion.isCompatibleWith(JavaVersion.VERSION_11)) {
-        include("plantuml-gplv2")
-    } else {
-        println("Skipping plantuml-gplv2 as it requires Java 11 or higher")
-    }
+//    if (javaVersion.isCompatibleWith(JavaVersion.VERSION_11)) {
+//        include("plantuml-gplv2")
+//    } else {
+//        println("Skipping plantuml-gplv2 as it requires Java 11 or higher")
+//    }
 } else {
     println("Not a CI build: only GPL will be generated")
 }
