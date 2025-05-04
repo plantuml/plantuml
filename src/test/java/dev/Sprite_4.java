3b5c75466ee76cd800de80c26b95acb167d66e18
Arnaud Roques <plantuml@gmail.com>
2025-05-04 20:10:30 +0200

    add tests

---
+++ src/test/java/dev/Sprite_4.java
@@ -0,0 +1,59 @@
+package dev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.GeneratedImage;
+import net.sourceforge.plantuml.SourceFileReader;
+import net.sourceforge.plantuml.preproc.Defines;
+
+/*
+ * 
+
+@startuml
+sprite bs1 <svg viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312z"/>
+</svg>
+legend <$bs1>
+:<$bs1>;
+-> sprite: <$bs1>;
+:a;
+-[#red]->sprite: <$bs1>;
+:b;
+@enduml
+
+
+ */
+public class Sprite_4 {
+
+	protected File getJavaFile() {
+		final String name = getClass().getName();
+		final File f = new File("src/test/java/" + name.replace('.', '/') + ".java");
+		return f;
+	}
+
+	@Test
+	public void testExecute() throws IOException, InterruptedException {
+		final File file = getJavaFile();
+		final FileFormatOption options = new FileFormatOption(FileFormat.PNG);
+
+		final File outputDirectory = new File("outputdev").getAbsoluteFile();
+		outputDirectory.mkdirs();
+
+		final SourceFileReader reader = new SourceFileReader(Defines.createWithFileName(file), file, outputDirectory,
+				Collections.<String>emptyList(), "UTF-8", options);
+		final List<GeneratedImage> list = reader.getGeneratedImages();
+
+		assertEquals(1, list.size());
+
+	}
+
+}
