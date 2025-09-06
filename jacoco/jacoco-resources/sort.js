77405423473d7e622e01985ea5cbc0895d93f847
arnaudroques <arnaudroques@users.noreply.github.com>
2025-09-06 10:00:02 +0000

    deploy: c396eb17d5448af0cdeebaa0dd50af1d02a23888

--- jacoco/jacoco-resources/sort.js
+++ jacoco/jacoco-resources/sort.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) 2009, 2025 Mountainminds GmbH & Co. KG and Contributors
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
