5a43f90d1a79986d705302057a5defe7a66319d5
Julian Jakob Dreykorn <47329537+fcnjd@users.noreply.github.com>
2025-11-18 16:47:53 +0100

    ✨ Add attributes to visibility modifiers in SVG (#2441)
    Co-authored-by: Julian Dreykorn <julian.dreykorn@inventivio.com>

--- src/main/java/net/sourceforge/plantuml/klimt/drawing/svg/SvgGraphics.java
+++ src/main/java/net/sourceforge/plantuml/klimt/drawing/svg/SvgGraphics.java
@@ -1262,6 +1262,7 @@ public class SvgGraphics {
 				case DATA_SOURCE_LINE:
 				case DATA_QUALIFIED_NAME:
 				case DATA_ENTITY_UID:
+				case DATA_VISIBILITY_MODIFIER:
 					pendingElements.get(0).setAttribute(typeIdent.getKey().getSvgKeyAttributeName(),
 							typeIdent.getValue());
 
@@ -1279,6 +1280,7 @@ public class SvgGraphics {
 				case DATA_UID:
 				case ID:
 				case DATA_SOURCE_LINE:
+				case DATA_VISIBILITY_MODIFIER:
 					pendingElements.get(0).setAttribute(typeIdent.getKey().getSvgKeyAttributeName(),
 							typeIdent.getValue());
 				}
