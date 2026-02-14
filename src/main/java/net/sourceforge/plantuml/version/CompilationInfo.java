651796d2d4a3774e5f0a2ee9fa0b51f7f2e3af06
Arnaud Roques <plantuml@gmail.com>
2026-02-14 15:57:01 +0100

    wip

---
+++ src/main/java/net/sourceforge/plantuml/version/CompilationInfo.java
@@ -0,0 +1,12 @@
+package net.sourceforge.plantuml.version;
+
+public class CompilationInfo {
+    
+    public static final String VERSION = "0.0.0";
+
+    public static final String COMMIT = "$git.commit.id$";
+
+    // placeholder: will be replaced during build when filtering is enabled
+    public static final long COMPILE_TIMESTAMP = 000L;
+
+}
