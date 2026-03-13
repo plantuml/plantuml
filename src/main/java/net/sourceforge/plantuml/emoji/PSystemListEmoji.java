cc41077d649088f491aae7ed50993bde07fb97e5
Arnaud Roques <plantuml@gmail.com>
2026-03-13 19:46:51 +0100

    wip

--- src/main/java/net/sourceforge/plantuml/emoji/PSystemListEmoji.java
+++ src/main/java/net/sourceforge/plantuml/emoji/PSystemListEmoji.java
@@ -35,83 +35,111 @@
  */
 package net.sourceforge.plantuml.emoji;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import net.sourceforge.plantuml.FileFormatOption;
-import net.sourceforge.plantuml.PlainDiagram;
+import net.sourceforge.plantuml.UgSimpleDiagram;
 import net.sourceforge.plantuml.core.DiagramDescription;
 import net.sourceforge.plantuml.core.UmlSource;
 import net.sourceforge.plantuml.klimt.UTranslate;
 import net.sourceforge.plantuml.klimt.drawing.UGraphic;
 import net.sourceforge.plantuml.klimt.font.StringBounder;
+import net.sourceforge.plantuml.klimt.geom.XDimension2D;
 import net.sourceforge.plantuml.klimt.shape.GraphicStrings;
 import net.sourceforge.plantuml.klimt.shape.TextBlock;
-import net.sourceforge.plantuml.klimt.shape.UDrawable;
 import net.sourceforge.plantuml.preproc.PreprocessingArtifact;
 
-public class PSystemListEmoji extends PlainDiagram {
-	
+public class PSystemListEmoji extends UgSimpleDiagram {
 
 	private final String text;
 
+	public PSystemListEmoji(UmlSource source, String text, PreprocessingArtifact preprocessing) {
+		super(source, preprocessing);
+		this.text = text;
+	}
+
 	@Override
-	protected UDrawable getRootDrawable(FileFormatOption fileFormatOption) throws IOException {
-		return new UDrawable() {
+	public XDimension2D calculateDimension(StringBounder stringBounder) {
+		return getTextBlock().calculateDimension(stringBounder);
+	}
+
+	@Override
+	public void drawU(UGraphic ug) {
+		getTextBlock().drawU(ug);
+	}
+
+	private TextBlock getTextBlock() {
+		final TextBlock header = GraphicStrings
+				.createBlackOnWhite(Arrays.asList("<b><size:16>Emoji available on Unicode Block " + text,
+						"(Blocks available: 26, 27, 1F3, 1F4, 1F5, 1F6, 1F9)"));
+
+		final Map<String, Emoji> some = new TreeMap<>();
+		for (Map.Entry<String, Emoji> ent : Emoji.getAll().entrySet())
+			if (ent.getKey().startsWith(text))
+				some.put(ent.getKey(), ent.getValue());
+
+		final List<TextBlock> items = new ArrayList<>();
+		for (Map.Entry<String, Emoji> ent : some.entrySet()) {
+			final String code = ent.getKey();
+			final String shortcut = ent.getValue().getShortcut();
+			final StringBuilder sb = new StringBuilder();
+			sb.append("<size:13>");
+			sb.append("\"\"<U+003C>:" + code + ":<U+003E> \"\"");
+			sb.append("<:" + code + ":>");
+			sb.append(" ");
+			sb.append("<#0:" + code + ":>");
+			if (shortcut != null) {
+				sb.append(" ");
+				sb.append("\"\"<U+003C>:" + shortcut + ":<U+003E> \"\"");
+			}
+			items.add(GraphicStrings.createBlackOnWhite(Arrays.asList(sb.toString())));
+		}
+
+		final int third = (items.size() + 2) / 3;
+
+		return new TextBlock() {
+			@Override
 			public void drawU(UGraphic ug) {
-				final TextBlock header = GraphicStrings
-						.createBlackOnWhite(Arrays.asList("<b><size:16>Emoji available on Unicode Block " + text,
-								"(Blocks available: 26, 27, 1F3, 1F4, 1F5, 1F6, 1F9)"));
 				header.drawU(ug);
 				final StringBounder stringBounder = ug.getStringBounder();
 				ug = ug.apply(UTranslate.dy(header.calculateDimension(stringBounder).getHeight()));
-
 				final UGraphic top = ug;
-
-				final Map<String, Emoji> some = new TreeMap<>();
-				for (Map.Entry<String, Emoji> ent : Emoji.getAll().entrySet())
-					if (ent.getKey().startsWith(text))
-						some.put(ent.getKey(), ent.getValue());
-
-				final int third = (some.size() + 2) / 3;
 				int i = 0;
-
-				for (Map.Entry<String, Emoji> ent : some.entrySet()) {
-					final String code = ent.getKey();
-					final String shortcut = ent.getValue().getShortcut();
-
-					final StringBuilder sb = new StringBuilder();
-					sb.append("<size:13>");
-					sb.append("\"\"<U+003C>:" + code + ":<U+003E> \"\"");
-					sb.append("<:" + code + ":>");
-					sb.append(" ");
-					sb.append("<#0:" + code + ":>");
-					if (shortcut != null) {
-						sb.append(" ");
-						sb.append("\"\"<U+003C>:" + shortcut + ":<U+003E> \"\"");
-					}
-
-					final TextBlock tmp = GraphicStrings.createBlackOnWhite(Arrays.asList(sb.toString()));
-					tmp.drawU(ug);
-					ug = ug.apply(UTranslate.dy(tmp.calculateDimension(stringBounder).getHeight()));
-
+				for (TextBlock item : items) {
+					item.drawU(ug);
+					ug = ug.apply(UTranslate.dy(item.calculateDimension(stringBounder).getHeight()));
 					i++;
 					if (i == third)
 						ug = top.apply(UTranslate.dx(500));
 					if (i == 2 * third)
 						ug = top.apply(UTranslate.dx(1000));
-
 				}
+			}
 
+			@Override
+			public XDimension2D calculateDimension(StringBounder stringBounder) {
+				final double headerHeight = header.calculateDimension(stringBounder).getHeight();
+				double col1Height = 0;
+				double col2Height = 0;
+				double col3Height = 0;
+				int i = 0;
+				for (TextBlock item : items) {
+					final double h = item.calculateDimension(stringBounder).getHeight();
+					if (i < third)
+						col1Height += h;
+					else if (i < 2 * third)
+						col2Height += h;
+					else
+						col3Height += h;
+					i++;
+				}
+				final double maxColHeight = Math.max(col1Height, Math.max(col2Height, col3Height));
+				return new XDimension2D(1500, headerHeight + maxColHeight);
 			}
 		};
-	}
-
-	public PSystemListEmoji(UmlSource source, String text, PreprocessingArtifact preprocessing) {
-		super(source, preprocessing);
-		this.text = text;
 	}
 
 	public DiagramDescription getDescription() {
--- src/main/java/net/sourceforge/plantuml/openiconic/PSystemListOpenIconic.java
+++ src/main/java/net/sourceforge/plantuml/openiconic/PSystemListOpenIconic.java
@@ -36,33 +36,29 @@
 package net.sourceforge.plantuml.openiconic;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.plantuml.FileFormatOption;
-import net.sourceforge.plantuml.PlainDiagram;
+import net.sourceforge.plantuml.UgDiagram;
 import net.sourceforge.plantuml.core.DiagramDescription;
 import net.sourceforge.plantuml.core.UmlSource;
 import net.sourceforge.plantuml.donors.PSystemDonors;
 import net.sourceforge.plantuml.klimt.geom.VerticalAlignment;
 import net.sourceforge.plantuml.klimt.shape.TextBlock;
 import net.sourceforge.plantuml.klimt.shape.TextBlockHorizontal;
-import net.sourceforge.plantuml.klimt.shape.UDrawable;
 import net.sourceforge.plantuml.openiconic.data.DummyIcon;
 import net.sourceforge.plantuml.preproc.PreprocessingArtifact;
 
-public class PSystemListOpenIconic extends PlainDiagram {
-	
+public class PSystemListOpenIconic extends UgDiagram {
 
 	public PSystemListOpenIconic(UmlSource source, PreprocessingArtifact preprocessing) {
 		super(source, preprocessing);
 	}
 
-	@Override
-	protected UDrawable getRootDrawable(FileFormatOption fileFormatOption) throws IOException {
+	public TextBlock getTextBlock12026(int num, FileFormatOption fileFormatOption) throws Exception {
 		final List<String> lines = new ArrayList<>();
 		lines.add("<b>List Open Iconic");
 		lines.add("<i>Credit to");
