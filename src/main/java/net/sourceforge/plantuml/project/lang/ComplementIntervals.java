9b59c7bcb96d426150d5c774174a96b542eed94b
Arnaud Roques <plantuml@gmail.com>
2026-04-09 23:01:27 +0200

    wip2

--- src/main/java/net/sourceforge/plantuml/project/lang/ComplementIntervals.java
+++ src/main/java/net/sourceforge/plantuml/project/lang/ComplementIntervals.java
@@ -37,9 +37,11 @@ package net.sourceforge.plantuml.project.lang;
 
 import java.time.LocalDate;
 
+import com.plantuml.ubrex.UMatcher;
 import com.plantuml.ubrex.builder.UBrexConcat;
 import com.plantuml.ubrex.builder.UBrexLeaf;
 import com.plantuml.ubrex.builder.UBrexNamed;
+import com.plantuml.ubrex.builder.UBrexOr;
 import com.plantuml.ubrex.builder.UBrexPart;
 
 import net.sourceforge.plantuml.project.DaysAsDates;
@@ -58,6 +60,11 @@ public class ComplementIntervals implements Something<GanttDiagram> {
 		return new RegexOr(toRegexB(suffix), toRegexE(suffix));
 	}
 
+	@Override
+	public UBrexPart toUnicodeBracketedExpressionComplement() {
+		return new UBrexOr(toRegexB(), toRegexE());
+	}
+
 	private IRegex toRegexB(String suffix) {
 		final DayPattern dayPattern1 = new DayPattern("1");
 		final DayPattern dayPattern2 = new DayPattern("2");
@@ -67,6 +74,18 @@ public class ComplementIntervals implements Something<GanttDiagram> {
 				Words.zeroOrMore(Words.THE), //
 				RegexLeaf.spaceOneOrMore(), //
 				dayPattern2.toRegex() //
+		);
+	}
+
+	private UBrexPart toRegexB() {
+		final DayPattern dayPattern1 = new DayPattern("1");
+		final DayPattern dayPattern2 = new DayPattern("2");
+		return UBrexConcat.build( //
+				dayPattern1.toUbrex(), //
+				Words.uexactly(Words.TO), //
+				Words.uzeroOrMore(Words.THE), //
+				UBrexLeaf.spaceOneOrMore(), //
+				dayPattern2.toUbrex() //
 		);
 	}
 
@@ -93,6 +112,20 @@ public class ComplementIntervals implements Something<GanttDiagram> {
 				new UBrexNamed("ECOUNT2", new UBrexLeaf("〇+〴d")));
 	}
 
+	@Override
+	public Failable<DaysAsDates> ugetMe(GanttDiagram project, UMatcher arg) {
+		final LocalDate d1 = new DayPattern("1").getDay(arg);
+		if (d1 != null) {
+			final LocalDate d2 = new DayPattern("2").getDay(arg);
+			return Failable.ok(new DaysAsDates(d1, d2));
+		}
+
+		if (arg.get("ECOUNT1", 0) != null)
+			return Failable.ok(resultE(project, arg));
+
+		throw new IllegalStateException();
+	}
+
 	public Failable<DaysAsDates> getMe(GanttDiagram project, RegexResult arg, String suffix) {
 		final LocalDate d1 = new DayPattern("1").getDay(arg);
 		if (d1 != null) {
@@ -112,6 +145,16 @@ public class ComplementIntervals implements Something<GanttDiagram> {
 		final TimePoint date1 = project.getMinTimePoint().addDays(day1);
 
 		final int day2 = Integer.parseInt(arg.get("ECOUNT2" + suffix, 0));
+		final TimePoint date2 = project.getMinTimePoint().addDays(day2);
+
+		return new DaysAsDates(date1.toDay(), date2.toDay());
+	}
+
+	private DaysAsDates resultE(GanttDiagram project, UMatcher arg) {
+		final int day1 = Integer.parseInt(arg.get("ECOUNT1", 0));
+		final TimePoint date1 = project.getMinTimePoint().addDays(day1);
+
+		final int day2 = Integer.parseInt(arg.get("ECOUNT2", 0));
 		final TimePoint date2 = project.getMinTimePoint().addDays(day2);
 
 		return new DaysAsDates(date1.toDay(), date2.toDay());
