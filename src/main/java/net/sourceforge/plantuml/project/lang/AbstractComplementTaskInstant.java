ef7198143d1aaf2a1ccd13a70bf413417dcefb55
Arnaud Roques <plantuml@gmail.com>
2026-05-25 17:54:06 +0200

    wip

--- src/main/java/net/sourceforge/plantuml/project/lang/AbstractComplementTaskInstant.java
+++ src/main/java/net/sourceforge/plantuml/project/lang/AbstractComplementTaskInstant.java
@@ -43,7 +43,6 @@ import net.sourceforge.plantuml.project.GanttDiagram;
 import net.sourceforge.plantuml.project.core.Moment;
 import net.sourceforge.plantuml.project.core.TaskAttribute;
 import net.sourceforge.plantuml.project.core.TaskInstant;
-import net.sourceforge.plantuml.regex.RegexResult;
 
 abstract class AbstractComplementTaskInstant implements Something<GanttDiagram> {
 
@@ -74,44 +73,6 @@ abstract class AbstractComplementTaskInstant implements Something<GanttDiagram> 
 
 			final boolean working = arg.get("COMPLEMENT_WORKING1", 0) != null
 					|| arg.get("COMPLEMENT_WORKING2", 0) != null;
-
-			final GanttConstraintMode mode = working ? GanttConstraintMode.DO_NOT_COUNT_CLOSE_DAY
-					: GanttConstraintMode.IGNORE_CALENDAR;
-
-			result = result.withDelta(delta, mode, system.getDefaultPlan());
-		}
-		return Failable.ok(result);
-	}
-
-	final public Failable<TaskInstant> getComplementTaskInstant(GanttDiagram system, RegexResult arg, String suffix) {
-		final String code = arg.get("COMPLEMENT_CODE_OTHER" + suffix, 0);
-		final String startOrEnd = arg.get("COMPLEMENT_START_OR_END" + suffix, 0);
-
-		final Moment task = system.getExistingMoment(code);
-		if (task == null)
-			return Failable.error("No such task " + code);
-
-		TaskInstant result = new TaskInstant(task, TaskAttribute.fromString(startOrEnd));
-		final String nb1 = arg.get("COMPLEMENT_NB1" + suffix, 0);
-		if (nb1 != null) {
-			final int factor1 = arg.get("COMPLEMENT_DAY_OR_WEEK1" + suffix, 0).startsWith("w") ? system.daysInWeek()
-					: 1;
-			final int days1 = Integer.parseInt(nb1) * factor1;
-
-			final String nb2 = arg.get("COMPLEMENT_NB2" + suffix, 0);
-			int days2 = 0;
-			if (nb2 != null) {
-				final int factor2 = arg.get("COMPLEMENT_DAY_OR_WEEK2" + suffix, 0).startsWith("w") ? system.daysInWeek()
-						: 1;
-				days2 = Integer.parseInt(nb2) * factor2;
-			}
-
-			int delta = days1 + days2;
-			if ("before".equalsIgnoreCase(arg.get("COMPLEMENT_BEFORE_OR_AFTER" + suffix, 0)))
-				delta = -delta;
-
-			final boolean working = arg.get("COMPLEMENT_WORKING1" + suffix, 0) != null
-					|| arg.get("COMPLEMENT_WORKING2" + suffix, 0) != null;
 
 			final GanttConstraintMode mode = working ? GanttConstraintMode.DO_NOT_COUNT_CLOSE_DAY
 					: GanttConstraintMode.IGNORE_CALENDAR;
